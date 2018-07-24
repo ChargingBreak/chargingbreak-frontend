3c46d6ccec2c780b7a0117e2770e5978c850e4a7
Mathias Hansen <me@codemonkey.io>
2018-07-23 22:21:59 -0400

    Added submit tip page

--- src/views/SubmitTip.vue
+++ src/views/SubmitTip.vue
@@ -2,7 +2,23 @@
   <div class="container">
     <h2>Submit tip</h2>
     <div v-if="user">
-      <button v-on:click="submit()">Submit</button>
+      <div class="form-group">
+        <label>What kind of tip do you have for this charger?</label>
+        <textarea name="text" class="form-control" v-model="formText"></textarea>
+      </div>
+
+      <div class="form-group">
+        <label>What is the category for this tip?</label>
+        <select name="theme" v-model="formTheme" class="form-control">
+          <option value="FOOD">Food</option>
+          <option value="KIDS">Kids</option>
+          <option value="RESTROOMS">Restrooms</option>
+          <option value="SHOPPING">Shopping</option>
+          <option value="ATMOSPHERE">Atmosphere</option>
+        </select>
+      </div>
+
+      <button v-on:click="submit()" class="btn btn-primary" :disabled="!formIsValid()">Submit</button>
     </div>
     <div v-else>
       <router-link to="/">Please log in</router-link> before submitting a tip
@@ -15,15 +31,27 @@ import { mapState } from 'vuex';
 
 export default {
   name: 'submittip',
+  data() {
+    return {
+      formTheme: 'FOOD',
+      formText: '',
+    };
+  },
   computed: mapState({
-    user: state => state.auth.user,
+    user: state => state.auth.user
   }),
   created() {
     this.$store.dispatch('auth/tryAutoSignIn');
   },
   methods: {
+    formIsValid() {
+      return this.formText.length > 0 && this.formTheme.length > 2;
+    },
     submit() {
-      this.$store.dispatch('chargers/submitTip', { id: this.$route.params.chargerId, text: 'Lorem ipsum dolor sit amet...', 'theme': 'FOOD' });
+      if (this.formIsValid()) {
+        this.$store.dispatch('chargers/submitTip', { id: this.$route.params.chargerId, text: this.formText, theme: this.formTheme });
+        this.$router.push({ path: `/charger/${this.$route.params.chargerId}` });
+      }
     }
   }
 };
